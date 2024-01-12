51a63dc02c7ee77d0595cbeb711907db4458b83f
agayevmr <Mahammad.Agayev@kapitalbank.az>
2024-01-12 20:35:32 +0400

    Adding chrome options to

--- src/test/java/createdriversession/CucumberHook.java
+++ src/test/java/createdriversession/CucumberHook.java
@@ -2,6 +2,7 @@ package createdriversession;
 
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import org.checkerframework.checker.units.qual.C;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -12,10 +13,10 @@ public class CucumberHook {
 
     @Before
     public void beforeScenario(){
-        ChromeOptions chromeOptions = new ChromeOptions();
-
-        driver = new ChromeDriver(chromeOptions);
-
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("headless");
+        options.addArguments("--ignore-certificate-errors");
+        driver = new ChromeDriver(options);
         driver.get("https://demos.telerik.com/aspnet-core/eshop");
     }
 
